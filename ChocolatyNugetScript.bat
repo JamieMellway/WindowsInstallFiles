c5022a8d42ca7a34f39e61068fc0ca1225adf6e0
Jamie Mellway <JamieMellway@users.noreply.github.com>
2014-08-10 03:30:38 -0400

    Create ChocolatyNugetScript.bat

---
+++ ChocolatyNugetScript.bat
@@ -0,0 +1,65 @@
+echo I'm currently trying chocolaty nuget.  This list will 
+
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
+
+installed
+=========
+choco install ChocolateyGUI  
+choco install javaruntime  
+choco install vlc
+choco install Firefox  
+choco install adblockplusfirefox  
+
+failed
+======
+choco install winamp
+choco install MicrosoftSecurityEssentials 
+
+still to try
+============
+choco install 7zip.install  
+choco install notepadplusplus  
+choco install GoogleChrome  
+choco install adblockpluschrome 
+choco install adblockplusie 
+choco install mpc-hc 
+choco install flashplayeractivex  
+choco install adobereader  
+choco install PowerShell  
+choco install ccleaner  
+choco install sysinternals  
+choco install python  
+choco install filezilla  
+choco install Console2  
+choco install skype  
+choco install fiddler  
+choco install paint.net  
+choco install windirstat  
+choco install calibre  
+choco install PDFCreator  
+choco install linqpad4  
+choco install AdobeAIR  
+choco install FoxitReader  
+choco install resharper  
+choco install dotPeek  
+choco install Silverlight  
+choco install k-litecodecpackbasic  
+choco install cdburnerxp  
+choco install kdiff3  
+choco install IrfanView  
+choco install uTorrent  
+choco install notepad2  
+choco install mingw  
+choco install NugetPackageExplorer  
+choco install imagemagick.app  
+choco install classic-shell  
+choco install googledrive  
+choco install imgburn  
+choco install speccy  
+choco install truecrypt  
+choco install steam  
+choco install githubforwindows  
+choco install TeraCopy  
+choco install daemontoolslite  
+
+
